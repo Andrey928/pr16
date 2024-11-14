9e7e9375b1909ab6df845579cd4cdcdc1d733833
Ilya <ig522222@mail.ru>
2024-11-14 17:19:09 +0300

    first commit

--- app/src/main/java/com/example/pr16/MainActivity.kt
+++ app/src/main/java/com/example/pr16/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.pr16
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.icu.text.IDNA.Info
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatDelegate
 
@@ -13,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
             val switch = findViewById<Switch>(R.id.switch1)
-
-
             switch.isChecked =
                 AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO
 
@@ -30,5 +32,49 @@ class MainActivity : AppCompatActivity() {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate. MODE_NIGHT_NO)
             }
         }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        updateMenuTheme(menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_menu_item-> {
+                toggleTheme()
+                return true
+            }
+            R.id.about_menu_item -> {
+                val intent = Intent(this@MainActivity, activity_info::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun toggleTheme() {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        recreate()
+    }
+
+    private fun updateMenuTheme(menu: Menu?) {
+        if (menu != null) {
+            val themeSwitch = menu.findItem(R.id.settings_menu_item)
+            if (themeSwitch != null) {
+                if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                    themeSwitch.title = "Сменить на светлую"
+                    themeSwitch.setIcon(R.drawable.icon)
+                } else {
+                    themeSwitch.title = "Сменить на тёмную"
+                    themeSwitch.setIcon(R.drawable.icon)
+                }
+            }
+        }
+    }
+
     }
 
